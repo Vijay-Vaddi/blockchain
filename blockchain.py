f606b30a3c260352b900d6304b71658e8606afbf
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-03-16 11:59:38 +0000

    mining and adding new block to chain

--- blockchain.py
+++ blockchain.py
@@ -94,7 +94,31 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return f"mine new block"
+    '''run PoW algorithm to get next proof'''
+    last_block = blockchain.last_block
+    last_proof= last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    '''receive reward for finding proof
+    Sender is '0' to indicate this node has mined the new coin'''
+    blockchain.new_transaction(
+        sender='0',
+        recipient=node_identifier,
+        amount=1
+    )
+
+    # create new block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message':'New block forged',
+        'index':block['index'],
+        'transactions': block['transactions'],
+        'proof':block['proof'],
+        'previous_hash':block['previous_hash'],
+    }
+    return jsonify(response), 200
 
 
 @app.route('/transactions/new', methods=['POST'])
@@ -107,7 +131,10 @@ def new_transaction():
         return 'Missing parameters', 400
     
     #create a new trax
-    return f"add new trax"
+    index = blockchain.new_transaction(values['sender'], values['recipient'],
+                                       values['amount'])
+    response = {'message': f"Adding transaction to the block {index}"}
+    return jsonify(response), 201
 
 
 @app.route('/chain', methods=['GET'])
